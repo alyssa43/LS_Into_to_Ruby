d07554e10298ba1df7a5967cdda37be8738cd7eb
Alyssa Easter <AlyssaStarley@gmail.com>
2023-06-01 12:20:31 -0700

    exercise 3 updated

--- Chapter1/the_basics/3.rb
+++ Chapter1/the_basics/3.rb
@@ -1,10 +1,10 @@
 movies = {
-  :dazed_and_confused => 1993
-  :titanic => 1997
-  :cinderella => 1950
-  :troy => 2004
-  :the_blind_side => 2009
+  dazed_and_confused: 1993,
+  titanic: 1997,
+  cinderella: 1950,
+  troy: 2004,
+  the_blind_side: 2009
 }
-movies.each { |key, value|
+movies.each do |key, value|
   puts value
-}
+end
