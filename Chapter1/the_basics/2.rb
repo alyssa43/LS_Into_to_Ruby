c3dc2ef8084d2e2da1be202f35de7b4f0e5b3c84
Alyssa Easter <AlyssaStarley@gmail.com>
2023-06-01 11:35:24 -0700

    Second exercise completed

---
+++ Chapter1/the_basics/2.rb
@@ -0,0 +1,9 @@
+thousands = 5678 / 1000
+hundreds = 5678 % 1000 / 100
+tens = 5678 % 100 / 10
+ones = 5678 % 10
+puts "Number: 5678"
+puts "Thousands: #{thousands}"
+puts "Hundreds: #{hundreds}"
+puts "Tens: #{tens}"
+puts "Ones: #{ones}"
